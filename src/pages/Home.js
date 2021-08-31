64750889a6d95d0d04c1b24afa5f02cde2fca0fa
Brian Murray <brianmurraysound@gmail.com>
2021-08-31 17:11:19 -0300

    Home page with some styling

---
+++ src/pages/Home.js
@@ -0,0 +1,48 @@
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { loadGames } from "../actions/gamesAction";
+import Game from "../components/Game";
+import styled from "styled-components";
+import { motion } from "framer-motion";
+
+const Home = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(loadGames());
+  }, [dispatch]);
+
+  const { popular, upcoming, newGames } = useSelector((state) => state.games);
+  return (
+    <GameList>
+      <h2>upcoming games</h2>
+      <Games>
+        {upcoming.map((game) => (
+          <Game
+            name={game.name}
+            released={game.released}
+            image={game.background_image}
+            id={game.id}
+            key={game.id}
+          ></Game>
+        ))}
+      </Games>
+    </GameList>
+  );
+};
+
+const GameList = styled(motion.div)`
+  padding: 0rem 5rem;
+  h2 {
+    padding: 5rem 0rem;
+  }
+`;
+
+const Games = styled(motion.div)`
+  min-height: 80vh;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+  grid-column-gap: 3rem;
+  grid-row-gap: 4rem;
+`;
+
+export default Home;
