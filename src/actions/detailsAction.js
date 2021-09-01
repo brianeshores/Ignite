eea6a943215c04cdc4a3e740718ce98f0783c003
Brian Murray <brianmurraysound@gmail.com>
2021-09-01 12:53:11 -0300

    Game details wait for loading

--- src/actions/detailsAction.js
+++ src/actions/detailsAction.js
@@ -2,6 +2,9 @@ import axios from "axios";
 import { gameDetailsUrl } from "../api";
 
 export const loadDetail = (id, short_screenshots) => async (dispatch) => {
+  dispatch({
+    type: "LOADING_DETAIL",
+  });
   const detailsData = await axios.get(gameDetailsUrl(id));
   dispatch({
     type: "GET_DETAIL",
--- src/components/GameDetails.js
+++ src/components/GameDetails.js
@@ -4,37 +4,43 @@ import { motion } from "framer-motion";
 import { useSelector } from "react-redux";
 
 const GameDetails = () => {
-  const { game, screenshots } = useSelector((state) => state.details);
+  const { game, screenshots, isLoading } = useSelector(
+    (state) => state.details
+  );
   return (
-    <CardShadow>
-      <Detail>
-        <Stats>
-          <div className="rating">
-            <h3>{game.name}</h3>
-            <p>{game.rating}</p>
-          </div>
-          <Info>
-            <h3>Platforms</h3>
-            <Platforms>
-              {game.platforms.map((data) => (
-                <h3 key={data.platform.id}>{data.platform.name}</h3>
+    <>
+      {!isLoading && (
+        <CardShadow>
+          <Detail>
+            <Stats>
+              <div className="rating">
+                <h3>{game.name}</h3>
+                <p>{game.rating}</p>
+              </div>
+              <Info>
+                <h3>Platforms</h3>
+                <Platforms>
+                  {game.platforms.map((data) => (
+                    <h3 key={data.platform.id}>{data.platform.name}</h3>
+                  ))}
+                </Platforms>
+              </Info>
+            </Stats>
+            <Media>
+              <img src={game.background_image} alt="" />
+            </Media>
+            <Description>
+              <p>{game.description_raw}</p>
+            </Description>
+            <div className="gallery">
+              {screenshots.map((screen) => (
+                <img key={screen.id} src={screen.image} alt="game" />
               ))}
-            </Platforms>
-          </Info>
-        </Stats>
-        <Media>
-          <img src={game.background_image} alt="" />
-        </Media>
-        <Description>
-          <p>{game.description_raw}</p>
-        </Description>
-        <div className="gallery">
-          {screenshots.map((screen) => (
-            <img key={screen.id} src={screen.image} alt="game" />
-          ))}
-        </div>
-      </Detail>
-    </CardShadow>
+            </div>
+          </Detail>
+        </CardShadow>
+      )}
+    </>
   );
 };
 
