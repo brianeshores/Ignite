af97796be869390637ef6a740626776c1f6e7df4
Brian Murray <brianmurraysound@gmail.com>
2021-09-01 14:23:26 -0300

    Images resized

--- src/components/GameDetails.js
+++ src/components/GameDetails.js
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import { motion } from "framer-motion";
 import { useSelector } from "react-redux";
 import { useHistory } from "react-router-dom";
+import { smallImage } from "../util";
 
 const GameDetails = () => {
   const history = useHistory();
@@ -38,15 +39,20 @@ const GameDetails = () => {
               </Info>
             </Stats>
             <Media>
-              <img src={game.background_image} alt="" />
+              <img src={smallImage(game.background_image, 1280)} alt="" />
             </Media>
             <Description>
               <p>{game.description_raw}</p>
             </Description>
             <div className="gallery">
-              {screenshots.map((screen) => (
-                <img key={screen.id} src={screen.image} alt="game" />
-              ))}
+              {screenshots &&
+                screenshots.map((screen) => (
+                  <img
+                    key={screen.id}
+                    src={smallImage(screen.image, 1280)}
+                    alt="game"
+                  />
+                ))}
             </div>
           </Detail>
         </CardShadow>
