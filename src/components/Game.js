00a55d0a48a4b149c5b14e24dd1ca2fe05f1838c
Brian Murray <brianmurraysound@gmail.com>
2021-09-01 13:34:51 -0300

    Game details toggle

--- src/components/Game.js
+++ src/components/Game.js
@@ -8,6 +8,7 @@ import { Link } from "react-router-dom";
 const Game = ({ name, released, image, id, screenshots }) => {
   const dispatch = useDispatch();
   const loadDetailsHandler = () => {
+    document.body.style.overflow = "hidden";
     dispatch(loadDetail(id, screenshots));
   };
   return (
@@ -27,6 +28,7 @@ const StyledGame = styled(motion.div)`
   text-align: center;
   border-radius: 1rem;
   cursor: pointer;
+  overflow: hidden;
   img {
     width: 100%;
     height: 40vh;
